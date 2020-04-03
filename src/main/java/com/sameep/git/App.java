abe4a4969f3d30e9abe13beb70d591d1c4e2c9be
sameepbohidar <sameep@gmail.com>
2020-04-03 10:52:12 +0530

    added if condition to check data

--- src/main/java/com/sameep/git/App.java
+++ src/main/java/com/sameep/git/App.java
@@ -5,6 +5,12 @@ public class App {
 	public void addData(String data)
 	{
 		System.out.println("data is:"+data);
+		
+		
+		if(data.equals("java"))
+		{
+			System.out.println("welcome");
+		}
 	}
 	
 	public static void main(String[] args) {
