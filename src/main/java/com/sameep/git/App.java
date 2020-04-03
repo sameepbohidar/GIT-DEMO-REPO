0fc9a59afc5c469831e16ecd3fcbea25df6179a2
sameepbohidar <sameep@gmail.com>
2020-04-03 10:48:39 +0530

    added gitdemo to local repo

---
+++ src/main/java/com/sameep/git/App.java
@@ -0,0 +1,14 @@
+package com.sameep.git;
+
+public class App {
+	
+	public void addData(String data)
+	{
+		System.out.println("data is:"+data);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+
+}
